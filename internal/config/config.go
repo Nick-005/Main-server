5901b0c03bbc869f697070701f4aabfc72721303
Nick <nikolay.borodin.1996@bk.ru>
2025-02-07 22:11:02 +0500

    try to deploy x2

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,7 +15,7 @@ type Config struct {
 }
 
 type HTTPServer struct {
-	Address     string        `yaml:"address" env-default:"localhost:8089"`
+	Address     string        `yaml:"address" env-default:"localhost:4000"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
